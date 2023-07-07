37f61539926d79e80a5c4424d8886dca6da372fa
tailwalk <jurlure@gmail.com>
2023-07-07 08:30:51 -0400

    final changes before continuation
    landing page is finished for nft gag. some changes to come to the tag, but mostly just adding functionality from here on out

---
+++ landing-page/main.js
@@ -0,0 +1,17 @@
+const counts = document.querySelectorAll('.count');
+const speed = 170
+counts.forEach((counter) =>{
+    function upData(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed
+        if(count < target){
+            counter.innerText = Math.floor(inc + count)
+            setTimeout(upData, 1)
+        }else{
+            counter.innerHTML = target
+        }
+    
+    }
+    upData()
+})
